83219bc19ba753b1a58d7f9a8006143b52ff6eb4
Nikolas Rummel <nikolasrummel6@gmail.com>
2023-07-21 01:53:08 +0200

    first commit

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,10 @@
+import {createClient} from "next-sanity/src";
+
+const config = {
+    projectId: "6elo1maj",
+    dataset: "production",
+    apiVersion: "2023-07-20",
+    useCdn: true
+}
+
+export default config;
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,5 @@
+import project from "@/sanity/schemas/project-schema";
+
+const schemas = [project]
+
+export default schemas;
