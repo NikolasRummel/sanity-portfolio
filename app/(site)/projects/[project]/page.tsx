1fa89d7e4f175a1bfab4bf4aca2d1663cb8ab1a0
Nikolas Rummel <nikolasrummel6@gmail.com>
2023-07-27 16:05:42 +0200

    refactored project content to match blog content structure

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -12,23 +12,56 @@ export default async function Project({params}: Props) {
     const slug = params.project;
     const project = await getProject(slug);
     const serialized = await serializeMdx(project.content);
-
+    const publishedOn = new Date(project._createdAt).toLocaleDateString(
+        "US",
+        {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }
+    );
     return (
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
 
             <header className="flex items-center justify-between">
                 <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{project.name}</h1>
-                <a href={project.url} title="View Project" target="_blank" rel="noopener noreferrer"
-                   className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition">
-                    View Project
-                </a>
             </header>
 
-            <Image src={project.image} alt={project.name} width={400} height={108}
-                   className="mt-10 border-2 border-gray-700 object-cover rounded-xl"/>
 
-            <div className="text-lg text-gray-700 mt-5">
-                <MdxContent source={serialized.serialized} />
+            <div>
+                <h1 className="my-16">
+                    <span className="block  mx-auto mt-2 text-4xl font-bold leading-10 text-center sm:text-5xl">
+                  {project.name}
+                </span>
+                    <span
+                        className="mt-2 block text-base font-semibold tracking-wide text-center text-sky-600 dark:text-teal-400 uppercase">
+                  {publishedOn}
+                </span>
+                </h1>
+
+                {project.image && (
+                    <div className="items-center">
+                        <Image
+                            src={project.image}
+                            alt={":("}
+                            width={4299}
+                            height={2499}
+                            priority
+                        />
+                    </div>
+                )}
+
+                <div className="flex justify-center mt-32">
+                    <a target="_blank" rel="noopener noreferrer"
+                       href={project.url}
+                       className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-teal-500 hover:text-teal-100 transition">
+                        View project on Github
+                    </a>
+                </div>
+
+                <div className={'mt-32'}>
+                    <MdxContent source={serialized.serialized}/>
+                </div>
             </div>
         </section>
     )
