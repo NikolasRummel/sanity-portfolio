5a56fe1023e7f2be44b5118c4897e06daeb6c2e6
Nikolas Rummel <nikolasrummel6@gmail.com>
2025-08-12 10:15:53 +0200

    updated timelne

--- components/TimelineContent.tsx
+++ components/TimelineContent.tsx
@@ -3,16 +3,25 @@
 import {Timeline, TimelineItem} from './TimeLine';
 
 const TimelineContent = () => {
-    const dmTECH = 'https://cdn.discordapp.com/attachments/786618658829893642/1132688291876044890/f49c3149-3283-4ac8-bedb-89d5bcfef0ad_1_645bba976b1a4.png';
-    const aes = 'https://cdn.discordapp.com/attachments/786618658829893642/1132688567764779148/AOPolaSJP1G_THpU4mzGoTu0zvntFZqV5MCgzw7XdvEs900-c-k-c0x00ffffff-no-rj.png';
-    const dhbw = 'https://cdn.discordapp.com/attachments/786618658829893642/1132687854766661672/dhbw_square.png';
 
     return (
         <Timeline>
-            <TimelineItem logo={dhbw}>
+            <TimelineItem logo={"/images/ntnu.png"}>
                 <TimelineItem.Title>
                     <a href="">
-                        <b>Studying computer science </b>&nbsp;<i> oct. 2023 - current</i>
+                        <b>Exchange Semester at NTNU, Trondheim, Norway </b>&nbsp;<i> Aug. 2025 - current</i>
+                    </a>
+                </TimelineItem.Title>
+
+                <TimelineItem.Description>
+                    I am currently attending an exchange semester at NTNU in Trondheim, Norway as part of my 5th semester. TODO
+                </TimelineItem.Description>
+            </TimelineItem>
+
+            <TimelineItem logo={"/images/dhbw.png"}>
+                <TimelineItem.Title>
+                    <a href="">
+                        <b>Studying computer science </b>&nbsp;<i> Oct. 2023 - current</i>
                     </a>
                 </TimelineItem.Title>
 
@@ -25,10 +34,10 @@ const TimelineContent = () => {
                 </TimelineItem.Description>
             </TimelineItem>
 
-            <TimelineItem logo={dmTECH}>
+            <TimelineItem logo={"/images/dmtech.png"}>
                 <TimelineItem.Title>
                     <a href="">
-                        <b>Software Engineer @dmTECH </b>&nbsp;<i> jan. 2023 - jul. 2023</i>
+                        <b>Software Engineer @dmTECH </b>&nbsp;<i> Jan. 2023 - jul. 2023</i>
                     </a>
                 </TimelineItem.Title>
 
@@ -38,19 +47,6 @@ const TimelineContent = () => {
                     version control. Taking the initiative to independently create a service was a rewarding and
                     enjoyable experience. Additionally, I familiarized myself with Scrum and project management
                     methodologies, further enhancing my software engineering skills.
-                </TimelineItem.Description>
-            </TimelineItem>
-
-            <TimelineItem last logo={aes}>
-                <TimelineItem.Title>
-                    <a href="">
-                        <b>School Seminar Paper </b>&nbsp;<i> aug. 2021 - aug. 2022</i>
-                    </a>
-                </TimelineItem.Title>
-                <TimelineItem.Description>
-                    The main idea of the Project was to develop a Minecraft server hosting platform,
-                    where a user should be able to host his own server and manage it with a web interface.
-                    The software was build with a microservice architecture based on REST.
                 </TimelineItem.Description>
             </TimelineItem>
 
